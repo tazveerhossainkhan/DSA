440df5e56832709db9319e1a335f6a0b4aafe906
tanveer9292 <tazveerhossainkhan009@gmail.com>
2023-05-16 20:41:35 +0600

    Intro to ArrayList

---
+++ src/com/muhit/ArrayListExample.java
@@ -0,0 +1,31 @@
+package com.muhit;
+
+import java.util.ArrayList;
+
+public class ArrayListExample {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(12);
+        list.add(132);
+        list.add(122);
+        list.add(142);
+        list.add(152);
+        list.add(162);
+        list.add(127);
+        list.add(12);
+        list.add(512);
+        list.add(122);
+        list.add(142);
+        list.add(12);
+        list.add(132);
+        list.add(12);
+        list.add(1278);
+        list.add(162);
+        list.add(152);
+        list.add(120);
+        list.add(1200);
+        list.add(17772);
+        System.out.println(list);
+
+    }
+}
